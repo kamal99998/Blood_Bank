1e25f45b072daafe4963bfa03010c29bffa00b2e
mredulhossain329 <mredulhossain7@gmail.com>
2026-08-16 12:39:20 +0600

    Updated server.js

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-//const express = require("express");
-//const cors = require("cors");
+const express = require("express");
+const cors = require("cors");
 
-//const app = express();
-//const PORT = process.env.PORT || 5001;
+const app = express();
+const PORT = process.env.PORT || 5001;
 
 // Middleware
-//app.use(cors());
-//app.use(express.json());
-/* app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // API routes
 const userRoutes = require("./routes/userRoutes");
@@ -59,4 +59,4 @@ app.use((err, req, res, next) => {
 // Start server
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Blood Bank API running on port ${PORT}`);
-});*/
+});
